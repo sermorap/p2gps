9bf360f4aa44e51ad48d9fd59cbd3e1cce0e6dbd
Sergio <sermorap@gmail.com>
2024-11-20 04:19:40 +0100

    b3

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
-while ! curl http://mongo:27017/
-do
-  echo "$(date) - still trying"
-  sleep 1
-done
-echo "$(date) - connected successfully"
-
-java -jar ordering-*.jar
+#!/bin/bash 
+ 
+while ! curl http://mongo:27017/ 
+do 
+  echo "$(date) - still trying" 
+  sleep 1 
+done 
+echo "$(date) - connected successfully" 
+ 
+java -jar /usr/local/app/ordering-*.jar 
